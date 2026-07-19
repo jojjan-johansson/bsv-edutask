f1b36da5b04881bfb1bd7444c18a00d5160669e8
Johanna Johansson <jojjan_johansson@hotmail.com>
2026-07-19 21:14:21 +0200

    Fix get user by email

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -4,17 +4,18 @@ from src.util.dao import DAO
 import re
 emailValidator = re.compile(r'.*@.*')
 
+
 class UserController(Controller):
     def __init__(self, dao: DAO):
         super().__init__(dao=dao)
 
     def get_user_by_email(self, email: str):
-        """Given a valid email address of an existing account, return the user object contained in the database associated 
+        """Given a valid email address of an existing account, return the user object contained in the database associated
         to that user. For now, do not assume that the email attribute is unique. Additionally print a warning message containing the email
         address if the search returns multiple users.
-        
+
         parameters:
-            email -- an email address string 
+            email -- an email address string
 
         returns:
             user -- the user object associated to that email address (if multiple users are associated to that email: return the first one)
@@ -30,11 +31,15 @@ class UserController(Controller):
 
         try:
             users = self.dao.find({'email': email})
-            if len(users) == 1:
-                return users[0]
-            else:
+
+            if len(users) == 0:
+                return None
+
+            if len(users) > 1:
                 print(f'Error: more than one user found with mail {email}')
-                return users[0]
+
+            return users[0]
+
         except Exception as e:
             raise
 
